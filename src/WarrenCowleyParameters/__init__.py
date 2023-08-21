6f35f4f5ffb5ef0dd3b2f9d252f627909132722b
Killian Sheriff <ksheriff@mit.edu>
2023-08-21 10:41:40 -0700

    update docstring

--- src/WarrenCowleyParameters/__init__.py
+++ src/WarrenCowleyParameters/__init__.py
@@ -7,6 +7,22 @@ from traits.api import Bool, Int, List
 
 
 class WarrenCowleyParameters(ModifierInterface):
+    """Compute the Warren-Cowley parameters.
+
+    ```python
+    from ovito.io import import_file
+    import WarrenCowleyParameters as WarrenCowleyParameters
+
+    pipeline = import_file("fcc.dump")
+    mod = WarrenCowleyParameters(nneigh=[0, 12, 18])
+    pipeline.modifiers.append(mod)
+    data = pipeline.compute()
+
+    wc_for_shells = data.attributes["Warren-Cowley parameters"]
+    print(f"1NN Warren-Cowley parameters: \n {wc_for_shells[0]}")
+    print(f"2NN Warren-Cowley parameters: \n {wc_for_shells[1]}")
+    ```
+    """
     # List of integers representing the maximum number of atoms in shells
     nneigh = List(Int, value=[0, 12], label="Max atoms in shells", minlen=2)
 
